84eabceb342009e1e92abc00a4380d63693ae602
Woojiseong <dnwltjd1234@uos.ac.kr>
2025-05-26 21:52:30 +0900

    1003번 문제 풀었음

--- Silver/S3/1003.py
+++ Silver/S3/1003.py
@@ -1,4 +1,26 @@
 '''
-1003
-피보나치
+피보나치 함수
 '''
+def count_fibonacci(N : int) -> int:
+    countZero = 0
+    countOne = 0
+    if N == 0:
+        return 1,0
+    elif N == 1:
+        return 0,1
+    else:
+        fibonacci = [1,1]
+        while N >= 3:
+            temp = fibonacci[0]+fibonacci[1]
+            fibonacci[0] = fibonacci[1]
+            fibonacci[1] = temp
+            N-=1
+        countZero = fibonacci[0]
+        countOne = fibonacci[1]
+
+    return countZero, countOne 
+
+testCase = int(input())
+for _ in range(testCase):
+    N = int(input())
+    print(*count_fibonacci(N))
